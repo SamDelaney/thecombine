199bf31fe45897871c7fa8ed9b99e9f4bf22e11d
SILMarkFuller <mark_fuller@sil.org>
2019-05-29 15:32:20 -0500

    backend api and framework rough draft

--- BackendFramework/Controllers/ValuesController.cs
+++ BackendFramework/Controllers/ValuesController.cs
@@ -2,44 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BackendFramework.ValueModels;
+using BackendFramework.WordService;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace BackendFramework.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+    [Produces("application/")]
+    [Route("v1/Collection")]
+    public class WordController : Controller
     {
-        // GET api/values
+        private readonly IWordService _wordService;
+        public WordController(IWordService wordService)
+        {
+            _wordService = wordService;
+        }
+        // GET: v1/collection
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new ObjectResult(await _wordService.GetAllWords());
+        }
+        // GET: v1/collection/name
+        [HttpGet("{name}", Name = "Get")]
+        public async Task<IActionResult> Get(string Id)
+        {
+            var word = await _wordService.GetWord(Id);
+            if (word == null)
+                return new NotFoundResult();
+            return new ObjectResult(word);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
+        // POST: v1/collection
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody]Word word)
         {
+            await _wordService.Create(word);
+            return new OkObjectResult(word.Id);
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        // PUT: v1/collection/5
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> Put(string Id, [FromBody]Word word)
         {
+            var document = await _wordService.GetWord(Id);
+            if (document == null)
+                return new NotFoundResult();
+            word.Id = document.Id;
+            await _wordService.Update(Id);
+            return new OkObjectResult(word);
         }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // DELETE: v1/ApiWithActions/5
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> Delete(string name)
         {
+            var document = await _wordService.GetWord(name);
+            if (gameFromDb == null)
+                return new NotFoundResult();
+            await _wordService.Delete(name);
+            return new OkResult();
         }
     }
+
 }
